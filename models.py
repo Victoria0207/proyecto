f1c6c413520a0c52c1b20104ce5a8d7606626a3f
Victoria0207 <92958513+Victoria0207@users.noreply.github.com>
2022-02-21 21:14:43 -0300

    Add files via upload

--- models.py
+++ models.py
@@ -2,12 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Persona(models.Model):
-
     id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=60)
     apellido = models.CharField(max_length=100)
-    dni=models.CharField(unique=True)
-    correo=models.DateField(default="2002","7","2")
+    dni=models.CharField(max_length=8)
+    fecha=models.DateField("2002","7","2")
     club = models.CharField(max_length=50)
     foto = models.ImageField(
         upload_to = 'foto/%Y/%m/%d',
